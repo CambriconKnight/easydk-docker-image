08e096db3348bfca1bc28713d0bbb44b84aa96d4
QMKnight <1197298090@qq.com>
2020-11-10 11:44:58 +0800

    add steps:manual copy(easydk)

--- build-image-easydk.sh
+++ build-image-easydk.sh
@@ -1,11 +1,6 @@
 #/bin/bash
 set -e
 
-# 1. copy your neuware package into the directory of easydk
-# 2. docker build -f dockerfile/Dockerfile --build-arg mlu_platform=MLU270 --build-arg neuware_package=neuware-mlu270-1.4.0-1_Ubuntu16.04_amd64.deb -t ubuntu_ffmpeg-mlu:v1 .
-# 3. docker run -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --privileged -v /dev:/dev --net=host --pid=host --ipc=host -v $HOME/.Xauthority -it --name ubuntu_ffmpeg-mlu  -v $PWD:/workspace ubuntu_ffmpeg-mlu:v1
-# 4. if docker is ready, can execute it by docker start ubuntu_ffmpeg-mlu / docker exec -it  ubuntu_ffmpeg-mlu /bin/bash
-
 #Version
 VERSION="v1.5.0"
 PATH_EASYDK_MLU="easydk"
@@ -19,15 +14,17 @@ green="\033[0;32m"
 red="\033[0;31m"
 yellow="\033[1;33m"
 
-##0.git clone
+##0.copy your easydk into the directory
 if [ ! -d "${PATH_EASYDK_MLU}" ];then
-    git clone http://172.10.80.76/video/easydk.git
     echo "Directory(${PATH_EASYDK_MLU}): Exists!"
+else
+    echo -e "${red}Directory(${PATH_EASYDK_MLU}): Not exist!${none}"
+    echo -e "${yellow}Copy your easydk into the directory[easydk-docker-image]!${none}"
+    echo -e "${yellow}eg:cp -v /data/ftp/demo/easydk ./${none}"
+    #Manual copy
+    #cp -v /data/ftp/demo/easydk ./
+    exit -1
 fi
-cd "${PATH_EASYDK_MLU}"
-# del .git
-find . -name ".git" | xargs rm -Rf
-cd ../
 
 ##copy your neuware package into the directory of easydk
 if [ -f "./${PATH_EASYDK_MLU}/${neuware_package_name}" ];then
